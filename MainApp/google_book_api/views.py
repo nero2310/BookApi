92f3cdad202d21ccce84d15304f36312013a4436
nero2310 <nero2310@wp.pl>
2021-04-26 00:56:19 +0200

    Setup app responsible for read data from google api.
    Add app to instaled apps,setup urls,add first view

--- MainApp/google_book_api/views.py
+++ MainApp/google_book_api/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 
 # Create your views here.
 
+class BookDetail(DetailView):
+
+    def get_context_data(self, **kwargs):
+        return 0
