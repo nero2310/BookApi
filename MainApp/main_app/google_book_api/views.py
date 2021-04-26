78ff5bc55fa75136fc1f92a5e56860c7b1805a97
nero2310 <nero2310@wp.pl>
2021-04-27 00:27:16 +0200

    WIP - Rebase this Add base view

--- MainApp/main_app/google_book_api/views.py
+++ MainApp/main_app/google_book_api/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.views.generic import DetailView
+
+import requests as rq
 
 
 # Create your views here.
@@ -8,3 +10,12 @@ class BookDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         return 0
+
+    def get_queryset(self):
+        pass
+
+    def get(self, request, *args, **kwargs):
+        response = rq.get("https://www.googleapis.com/books/v1/volumes?q=search+Witcher")
+        print(response)
+        return HttpResponse(response.json()["items"])
+
