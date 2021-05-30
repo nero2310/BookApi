07e539c87a906d058f2c40fea9b81520a3bcaa4b
nero2310 <nero2310@wp.pl>
2021-05-30 13:12:43 +0200

    Add tests to testing creating urls to google book api endpoint

--- MainApp/main_app/google_book_api/tests.py
+++ MainApp/main_app/google_book_api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .views import GoogleBookApi
 
 # Create your tests here.
+
+class GoogleBookApiUrlTest(TestCase):
+
+    def test_search_url_without_parameter(self):
+        book_1 = GoogleBookApi()
+        book_1.search_parameters()
+        self.assertEqual(book_1._get_query_url(),"https://www.googleapis.com/books/v1/volumes?q=") # Base url
+
+    def test_search_url_with_parameter(self):
+        book_1 = GoogleBookApi()
+        book_1.search_parameters()
+        self.assertHTMLEqual()
