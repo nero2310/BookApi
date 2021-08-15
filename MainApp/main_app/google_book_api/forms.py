067a5c596b3d12fbc1da119f3a4c38158c247b49
nero <nero@MacBook-Air-nero.local>
2021-08-15 18:31:03 +0200

    Remove fields from search form

--- MainApp/main_app/google_book_api/forms.py
+++ MainApp/main_app/google_book_api/forms.py
@@ -1,8 +1,9 @@
 from django.forms import ModelForm
 from .views import GoogleBookApi
 
+
 class GoogleSearchForm(ModelForm):
     class Meta:
         model = GoogleBookApi
-        fields = '__all__'
+        fields = ('title', 'authors')
         success_url = '/book/details'
