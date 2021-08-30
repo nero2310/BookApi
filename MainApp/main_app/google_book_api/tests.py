61d6f305bfde55b93a81d39526def66ef97ee98b
nero2310 <nero2310@wp.pl>
2021-08-30 22:41:21 +0200

    Remove tests whose use deleted GoogleBookApi

--- MainApp/main_app/google_book_api/tests.py
+++ MainApp/main_app/google_book_api/tests.py
@@ -1,19 +1,8 @@
 from django.test import TestCase,Client
-from .views import GoogleBookApi,ApiQueryGenerator
+from .views import ApiQueryGenerator
 
 # Create your tests here.
 
-class GoogleBookApiUrlTest(TestCase):
-
-    def test_search_url_without_parameter(self):
-        book_1 = GoogleBookApi()
-        book_1.search_parameters()
-        self.assertEqual(book_1._get_query_url(),"https://www.googleapis.com/books/v1/volumes?q=") # Base url
-
-    def test_search_url_with_parameter(self):
-        book_1 = GoogleBookApi()
-        book_1.search_parameters()
-        self.assertHTMLEqual("","")
 
 class GoogleBookApiClientTest(TestCase):
     def setUp(self):
