eba0db9abc69632a32026aae972e7b341be5d89d
nero2310 <nero2310@wp.pl>
2021-09-05 21:57:42 +0200

    Add Author Model

--- MainApp/main_app/google_book_api/models.py
+++ MainApp/main_app/google_book_api/models.py
@@ -7,12 +7,23 @@ import requests
 # Create your models here.
 
 
+class Author(models.Model):
+
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    death_date = models.DateField(blank=True,null=True)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
 
     title = models.CharField(max_length=100)
     kind = models.CharField(blank=True, max_length=100)
     api_book_id = models.CharField(blank=True, max_length=100)
-    authors = models.JSONField()
+    authors = models.ManyToManyField(Author)
     publisher = models.CharField(blank=True, max_length=200)
     publish_data = models.DateField(blank=True,null=True)
     isbn = models.CharField(blank=True,max_length=13)
