bc3c8e178cda220d8fbd711e8fb28db6f168ebbe
nero2310 <nero2310@wp.pl>
2021-09-05 15:33:41 +0200

    Create test to test generating api query for multiple authors

--- MainApp/main_app/google_book_api/tests.py
+++ MainApp/main_app/google_book_api/tests.py
@@ -26,11 +26,15 @@ class QueryLinkGenerationTest(TestCase):
         url = test.generate_query()
         self.assertEqual(url, "https://www.googleapis.com/books/v1/volumes?q=")
 
-    def test_query_with_authors(self):
+    def test_query_with_author(self):
         test = ApiQueryGenerator(**{"author": "Martin"})
         url = test.generate_query()
         self.assertEqual(url, "https://www.googleapis.com/books/v1/volumes?q=inauthor:Martin")
 
+    def test_query_with_multiple_authors(self):
+        test = ApiQueryGenerator(**{"author":["A","B","C"]})
+        url = test.generate_query()
+        self.assertEqual(url, "https://www.googleapis.com/books/v1/volumes?q=inauthor:A+inauthor:B+inauthor:C")
 
 class BookSearchFormTest(TestCase):
 
