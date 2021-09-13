f8fa0715ce168c79a35ca61d6b5c79f15d4fe69d
nero2310 <nero2310@wp.pl>
2021-09-13 23:25:54 +0200

    WIP on author model

--- MainApp/main_app/authors/admin.py
+++ MainApp/main_app/authors/admin.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 from google_book_api.models import Book
 from .models import Author
 
+
 # Register your models here.
+
 class AuthorToBookInline(admin.StackedInline):
     model = Book.authors.through
+
 
 class AdminAuthor(admin.ModelAdmin):
     inlines = [
         AuthorToBookInline
     ]
+
 
 admin.site.register(Author, AdminAuthor)
