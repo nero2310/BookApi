4983ea355de619a6540ea51572fde97877282fec
nero2310 <nero2310@wp.pl>
2021-09-14 22:52:45 +0200

    Create base Author View

--- MainApp/main_app/authors/views.py
+++ MainApp/main_app/authors/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
 # Create your views here.
+
+class AuthorView(DetailView):
+
+    def get(self,request,args,**kwargs):
+        context = {}
+        return render(request, "authors/author_detail_view.html", context=context)
--- MainApp/main_app/google_book_api/views.py
+++ MainApp/main_app/google_book_api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views.generic import DetailView
 
 from .forms import GoogleSearchForm
