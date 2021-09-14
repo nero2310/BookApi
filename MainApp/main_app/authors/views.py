7038fd410058a8b11120eeed257a3e27e206cd1d
nero2310 <nero2310@wp.pl>
2021-09-14 23:24:50 +0200

    Pass to template all Authors Book

--- MainApp/main_app/authors/views.py
+++ MainApp/main_app/authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 
 from .models import Author
+from google_book_api.models import Book
 
 
 # Create your views here.
@@ -10,10 +11,12 @@ class AuthorDetail(DetailView):
 
     def get(self, request, slug, *args, **kwargs):
         AuthorModel = Author.objects.get(slug=slug)
+        Books = AuthorModel.book_set.all()
         context = {
             'author_name' : AuthorModel.name,
             'biography': AuthorModel.biography,
             'birth_date': AuthorModel.birth_date,
             'death_date': AuthorModel.death_date,
+            'books' : Books,
         }
         return render(request, "authors/author_detail_view.html", context=context)
