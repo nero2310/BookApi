f4792e2a4ebc89a2587ce04241c8e87a35380f72
nero2310 <nero2310@wp.pl>
2021-10-31 23:53:47 +0100

    Redirect user to book detailed page
    Redirect user to book page when thumbnail
    is clicked

--- MainApp/main_app/google_book_api/urls.py
+++ MainApp/main_app/google_book_api/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('search', book_search),
     path('search-results/<str:title>', book_search_results, name = 'search_results'),
     path('search-results/<str:title>/<str:author>', book_search_results, name = 'search_results'),
-    # path('serarch-results/<str:title>', BookSearch.as_view()),
     path('library', LibraryDetail.as_view()),
-    path('<slug:slug>', book_detail_view),
+    path('<slug:slug>', book_detail_view, name = 'single_book_view'),
 ]
