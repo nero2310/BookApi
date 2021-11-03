86e5cf63816daa49c7884e3f47a66d6a47475e90
nero2310 <nero2310@wp.pl>
2021-11-03 22:31:55 +0100

    Add to navbar login/logout system, add JS support

--- MainApp/main_app/user_auth/urls.py
+++ MainApp/main_app/user_auth/urls.py
@@ -7,5 +7,5 @@ app_name = 'user_auth'
 urlpatterns = [
     path('login/', views.login_view, name='login_view'),
     path('register/', views.register_view),
-    path('logout/', views.logout_user),
+    path('logout/', views.logout_user, name = 'logout_view'),
 ]
