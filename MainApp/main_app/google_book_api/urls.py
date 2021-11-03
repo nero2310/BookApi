b17b89eeb1057aadabab5af2fafb213f0921026c
nero2310 <nero2310@wp.pl>
2021-11-03 22:49:11 +0100

    Fix bug, now properly resolve url to Book Search

--- MainApp/main_app/google_book_api/urls.py
+++ MainApp/main_app/google_book_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import book_search, book_search_results, book_detail_view, LibraryDetail
 
 urlpatterns = [
-    path('search', book_search),
+    path('search', book_search, name = 'search_view'),
     path('search-results/<str:title>', book_search_results, name = 'search_results'),
     path('search-results/<str:title>/<str:author>', book_search_results, name = 'search_results'),
     path('library', LibraryDetail.as_view()),
